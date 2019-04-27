4fa6d108bc6983900e72ce04a7828b359174df83
bdamay <bdamay@gmail.com>
2019-04-27 10:39:57 +0200

    readding frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'BCF Manager VueJS Frontend '
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
